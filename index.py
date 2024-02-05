940ce096986acb76351ee3454bac17a2550c0b1f
Werli <47005686+werlipina@users.noreply.github.com>
2024-02-05 22:16:49 +0000

    Great update to index.py
    Basically is this creates both "Images" and "PDF" folders if they don't already exist, to make life more easier. It checks if there are any .JPG or .PNG files in the "Images" folder, printing a warning message and stopping the execution if none are found. but you can add more extensions modifying the 17 line... Finally I made it save the date and time the PDF was create for the sake of organization,..

--- index.py
+++ index.py
@@ -1,20 +1,34 @@
 import os
+from datetime import datetime
 from PIL import Image
-    # Check if Images directory exists already in the root folder
-if not os.path.exists("Images"):
-    # Create Images directory at the root folder if it doesn't exist
-    os.makedirs("Images")
-    print("Directory 'Images' created successfully.")
-def ReadDirList(ImageFolder):
-    DirList = os.listdir(ImageFolder)
-    DirList.sort()
-    return DirList
-    # {1} I'll enhance this code later...
-def Img2PDF(ImageFolder, PDFName):
-    image = [ Image.open(f"{ImageFolder}/" + str(f)) for f in ReadDirList(ImageFolder)]
-    image[0].save(f"{PDFName}.pdf", "PDF",resolution=100.0, save_all=True, append_images=image[1:])
-    # Fix bellow! Make it the .pdf file name with a relevant name {1}... Maybe random?
+
 if __name__ == "__main__":
+    # Check if Image and PDF folders exist, create them if not
+    if not os.path.exists("Images"):
+        os.makedirs("Images")
+        print(f"Directory 'Images' created successfully.")
+    if not os.path.exists("PDF"):
+        os.makedirs("PDF")
+        print(f"Directory 'PDF' created successfully.")
+
     ImageFolder = "Images"
-    PDFName = "Image2PDF.pdf"
-    Img2PDF(ImageFolder, PDFName)
+
+    # Check if there are JPG or PNG files in the directory
+    if not any([os.path.exists(os.path.join(ImageFolder, f)) for f in os.listdir(ImageFolder) if (f.endswith(".jpg") or f.endswith(".png"))]):
+        print("No image files found in 'Images' directory. Add some!")
+        exit() # Stop the execution upon discovering no valid images.
+
+    def ReadDirList(ImageFolder):
+        DirList = os.listdir(ImageFolder)
+        DirList.sort()
+        return DirList
+
+    def Img2PDF(ImageFolder, PDFName=None): # Parameter updated to accept None as default value...
+        if not PDFName:
+            current_datetime = datetime.now().strftime("%Y-%m-%d-%H%M%S")
+            PDFName = f"PDF/{ImageFolder}_PDF_{current_datetime}.pdf"     # Generate a new file name based on date, time, sec, source directory name and default prefix 'PDF'. Very useful!
+            print(f"Everything is done!")
+        image = [ Image.open(f"{ImageFolder}/" + str(f)) for f in ReadDirList(ImageFolder)]
+        image[0].save(PDFName, "PDF", resolution=100.0, save_all=True, append_images=image[1:])
+
+    Img2PDF(ImageFolder)  # PDFName set as None by default here; the function will create a custom name automatically using source directory name for uniqueness...
