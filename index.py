fb96574d28ad53b3446b000474c93ff7c4480e2b
Werli <47005686+werlipina@users.noreply.github.com>
2024-02-06 20:12:09 +0000

    Update index.py
    Quick fix

--- index.py
+++ index.py
@@ -3,22 +3,22 @@ from datetime import datetime
 from PIL import Image
 
 if __name__ == "__main__":
-    # Check if Images and PDF folders exist, create them if not
+    # Check if Image and PDF folders exist, create them if not
     if not os.path.exists("Images"):
         os.makedirs("Images")
-        print(f"Directory 'Images' created successfully.")
+        print(f"Folder 'Images' created successfully.")
     if not os.path.exists("PDF"):
         os.makedirs("PDF")
-        print(f"Directory 'PDF' created successfully.")
-
+        print(f"Folder 'PDF' created successfully.")
+        
     ImageFolder = "Images" # Source Folder.
-    
-while True: # Added a loop which instead of stopping code execution, it will wait until you save JPG or PNG files to "Images" folder and press Enter.
+
+    while True: # Added a loop which instead of stopping code execution, it will wait until you save JPG or PNG files to "Images" folder and press Enter.
         # Bellow it will check if there are JPG or PNG files in the folder.
         if any([os.path.exists(os.path.join(ImageFolder, f)) for f in os.listdir(ImageFolder) if (f.endswith(".jpg") or f.endswith(".png"))]):
             break
         print("No image files found in 'Images' folder.")
-        print("When you're ready, press Enter to continue...")
+        print("When you're ready, press Enter to continue.")
         input()
 
     DirList = os.listdir(ImageFolder)
@@ -27,9 +27,9 @@ while True: # Added a loop which instead of stopping code execution, it will wai
     def Img2PDF(ImageFolder, PDFName=None): # Parameter updated to accept None as default value
         if not PDFName:
             current_datetime = datetime.now().strftime("%Y-%m-%d-%H%M%S")
-            PDFName = f"PDF/{ImageFolder}_PDF_{current_datetime}.pdf"      # Generate a new file name based on date, time, sec, source directory name and default prefix 'PDF'.
-            print(f"Everything is done!")
+            PDFName = f"PDF/{ImageFolder}_PDF_{current_datetime}.pdf" # Generate a new file name based on date, time, sec, source folder name and default prefix 'PDF'.
+            # print(f"Wait...") <---- Will fix it later...
         image = [ Image.open(f"{ImageFolder}/" + str(f)) for f in DirList]
         image[0].save(PDFName, "PDF", resolution=100.0, save_all=True, append_images=image[1:])
 
-    Img2PDF(ImageFolder)  # PDFName set as None by default here; the function will create a custom name automatically using source directory name for uniqueness...
+    Img2PDF(ImageFolder)  # PDFName set as None by default here; the function will create a custom name automatically using source folder name for uniqueness...
